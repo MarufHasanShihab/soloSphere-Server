5024696d33495270f4d146203cbf4711cfb79c67
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-28 11:45:27 +0600

    connect mogodb database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,31 @@ app.use(cors());
 app.use(express());
 
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+    }
+  }
+  run().catch(console.dir);
+
+
 app.get("/",(req,res)=>{
     res.send("Server Is Runningg")
 })
 
 app.listen(port,()=>console.log(`Your Server Is Running In ${port} Port`));
+
+// xHK0IgPg87L31Zz2 soloSphere
