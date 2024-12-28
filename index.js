2cb82b2ea0a9a7c64362f28c6647d61f78ea850b
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-28 14:29:34 +0600

    added feature the job data find to mongodb database

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const client = new MongoClient(process.env.MONGODB_URI, {
   });
   async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
+      const jobsCollection = client.db("soloSphere").collection("jobs");
+
+      // jobs related api
+      app.get("/jobs",async(req,res)=>{
+        const result = await jobsCollection.find().toArray();
+        res.send(result);
+      })
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
